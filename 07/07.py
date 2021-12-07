b7aa5e01ee67300fbb6c855caf99d798b1aa0727
Florian Lercher <florian.lercher@tum.de>
2021-12-07 11:52:58 +0100

    Add solution for day 07 part 2

--- 07/07.py
+++ 07/07.py
@@ -5,22 +5,25 @@ import numpy as np
 
 with open("./input", "r") as f:
     for l in f:
-        positions = [float(x) for x in l.strip().split(",")]
+        positions = [int(x) for x in l.strip().split(",")]
         break
 
 input = torch.tensor(positions)
-align_pos = torch.tensor(input.mean(), requires_grad=True)
-loss_fn = torch.nn.L1Loss(reduction="sum")
+align_pos = torch.tensor(np.mean(positions), requires_grad=True)
+loss_part1 = torch.nn.L1Loss(reduction="sum")
+def loss_part2(y, t):
+    diff = torch.abs(y - t)
+    return torch.sum(diff * (diff + 1) / 2)
 
 lr = 0.1
 optimizer = torch.optim.Adam((align_pos,), lr=lr)
 
 for t in range(10000):
-    loss = loss_fn(align_pos, input)
+    loss = loss_part2(align_pos, input)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 align = align_pos.item()
 print(f"Aligning at position: {align} Fuel required: {loss.item()}")
-print(f"Ceil: Aligning at position: {math.ceil(align)} Fuel required: {torch.abs(input - math.ceil(align)).sum()}")
-print(f"Floor: Aligning at position: {math.floor(align)} Fuel required: {torch.abs(input - math.floor(align)).sum()}")
+print(f"Ceil:  Aligning at position: {math.ceil(align)} Fuel required: {loss_part2(math.ceil(align), input).item()}")
+print(f"Floor: Aligning at position: {math.floor(align)} Fuel required: {loss_part2(math.floor(align), input).item()}")
