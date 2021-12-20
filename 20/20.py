9365919c55d654ffe1501e16b74b56ba57d49bbd
Florian Lercher <florian.lercher@tum.de>
2021-12-20 11:27:38 +0100

    Add solution for day 20 part 2

--- 20/20.py
+++ 20/20.py
@@ -2,7 +2,8 @@ from functools import reduce
 import numpy as np
 from scipy.ndimage import generic_filter
 
-class ImageEnhanceer:
+
+class ImageEnhancer:
     def __init__(self, enhancement_algorithm) -> None:
         if len(enhancement_algorithm) != 512:
             raise ValueError("Algorithm has to have 512 elements")
@@ -14,38 +15,48 @@ class ImageEnhanceer:
 
     def enhance(self, image, steps=2):
         if self.algorithm[0] != 0 and steps % 2 != 0:
-            raise ValueError("Can only perform an even amount of steps")
+            raise ValueError(
+                "Can only perform an even amount of steps, otherwise there would be an infinite amount of light pixels"
+            )
         if self.algorithm[0] != 0 and self.algorithm[-1] != 0 and steps > 0:
             raise ValueError("Image would have infinite light pixels")
 
         for i in range(steps):
-            if i % 2 == 0 or self.algorithm[0] == 0:
+            if i % 2 == 0:
                 padding = 0
             else:
                 padding = self.algorithm[0]
+            # Pad by 1: These pixels are not only surrounded by default values, so their value might differ from the default in the next step
             image = np.pad(image, 1, constant_values=padding)
-            image = generic_filter(image, self.enhance_filter, size=3, mode="constant", cval=padding)
+            image = generic_filter(
+                image, self.enhance_filter, size=3, mode="constant", cval=padding
+            )
 
         return image
 
+
 def make_int(c):
-    if c == '#':
+    if c == "#":
         return 1
-    elif c == '.':
+    elif c == ".":
         return 0
     else:
         raise ValueError(f"Cannot match char {c}")
 
+
 def main():
     with open("./input", "r") as f:
         algorithm = np.array([make_int(c) for c in next(f).strip()])
-        next(f) # skip blank line
+        next(f)  # Skip blank line
         image = np.array([[make_int(c) for c in l.strip()] for l in f if l != ""])
 
-    enhancer = ImageEnhanceer(algorithm)
+    enhancer = ImageEnhancer(algorithm)
+    # Enhance 2 times
     image = enhancer.enhance(image)
-    print(image)
-    print(image.sum())
+    print(f"After enhancing  2 times there are {image.sum():5d} light pixels")
+    # Enhance 48 more times --> 50 enhancements in total
+    image = enhancer.enhance(image, steps=48)
+    print(f"After enhancing 50 times there are {image.sum():5d} light pixels")
 
 
 if __name__ == "__main__":
